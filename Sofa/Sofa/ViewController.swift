83fdfb8a7d6edcc0ca51bcebca4835223de01139
robinlietar <robinlietar@MacBook-Pro-de-Robin-2.local>
2016-10-30 20:37:15 +0100

    Creation of project

---
+++ Sofa/Sofa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sofa
+//
+//  Created by Robin Lietar on 30/10/2016.
+//  Copyright © 2016 Sofa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
